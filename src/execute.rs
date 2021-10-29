029b676c34067560b90737bb2d5a8fff29e1d91c
Vlad <vladjdk@gmail.com>
2021-10-29 16:19:38 -0400

    Check minted + cleaner map update

--- src/execute.rs
+++ src/execute.rs
@@ -145,15 +145,11 @@ pub fn execute_change_color(
     position: u16,
     color_map: [[Color; 5]; 5],
 ) -> Result<Response, ContractError> {
-    let owner_check = get_owner(deps.as_ref(), env, position);
-    let owner;
-
-    match owner_check {
-        None => {
-            return Err(ContractError::DoesNotExist {});
-        }
-        Some(_) => owner = owner_check.unwrap(),
+    if !token_minted(deps.as_ref(), env.clone(), position) {
+        return Err(ContractError::DoesNotExist {});
     }
+
+    let owner = get_owner(deps.as_ref(), env, position).unwrap();
 
     if owner != info.sender {
         return Err(ContractError::Unauthorized {});
@@ -162,7 +158,7 @@ pub fn execute_change_color(
     let token_id = position.to_string();
     let token = tokens().load(deps.storage, &token_id)?;
 
-    let extension = token.extension;
+    let extension = token.clone().extension;
 
     let updated_extension = PixelExtension {
         pixel_colors: color_map,
@@ -171,15 +167,12 @@ pub fn execute_change_color(
 
     let updated_token = TokenInfo::<PixelExtension> {
         owner: Addr::unchecked(owner),
-        approvals: token.approvals,
-        token_uri: token.token_uri,
+        approvals: token.clone().approvals,
+        token_uri: token.clone().token_uri,
         extension: updated_extension,
     };
 
-    tokens().update(deps.storage, &token_id, |existing| match existing {
-        None => Err(ContractError::Unauthorized {}),
-        Some(_) => Ok(updated_token),
-    })?;
+    tokens().replace(deps.storage, &token_id, Some(&updated_token), Some(&token))?;
 
     Ok(Response::new()
         .add_attribute("action", "change color")
